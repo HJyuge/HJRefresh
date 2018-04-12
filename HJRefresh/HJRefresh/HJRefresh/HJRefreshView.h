1f71ab571fafe90419a18e978d5021c3f5a4b31d
HJyuge <472502790@qq.com>
2018-04-13 00:29:23 +0800

    create refresh view

---
+++ HJRefresh/HJRefresh/HJRefresh/HJRefreshView.h
@@ -0,0 +1,13 @@
+//
+//  HJRefreshView.h
+//  HJRefresh
+//
+//  Created by huangjin on 2018/4/12.
+//  Copyright © 2018年 huangjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HJRefreshView : UIView
+
+@end
---
+++ HJRefresh/HJRefresh/HJRefresh/HJRefreshView.m
@@ -0,0 +1,21 @@
+//
+//  HJRefreshView.m
+//  HJRefresh
+//
+//  Created by huangjin on 2018/4/12.
+//  Copyright © 2018年 huangjin. All rights reserved.
+//
+
+#import "HJRefreshView.h"
+
+@implementation HJRefreshView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
