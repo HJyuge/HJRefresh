5f7113b017264fbdf28a928ada8f2d6a189e4215
HJyuge <472502790@qq.com>
2018-04-12 16:48:24 +0800

    create project

---
+++ HJRefresh/HJRefresh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJRefresh
+//
+//  Created by huangjin on 2018/4/12.
+//  Copyright © 2018年 huangjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HJRefresh/HJRefresh/RefreshTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RefreshTableViewController.h
+//  HJRefresh
+//
+//  Created by huangjin on 2018/4/12.
+//  Copyright © 2018年 huangjin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RefreshTableViewController : UITableViewController
+
+@end
